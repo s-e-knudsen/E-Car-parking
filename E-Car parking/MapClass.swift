3a3ba435f0ccb7d2dcb95653f3d72c1add92efcd
SorenEKnudsen <soren.egede.knudsen@gmail.com>
2018-03-05 13:43:51 +0100

    No works with array and parsing from jason file. Json i local at this time.

--- E-Car parking/MapClass.swift
+++ E-Car parking/MapClass.swift
@@ -12,8 +12,9 @@ import MapKit
 class MapClass: NSObject, MKAnnotation {
     let title: String?
     let locationName: String
-    let discipline: String
+    //let discipline: String
     let coordinate: CLLocationCoordinate2D
+   
     
     //    // markerTintColor for disciplines: Sculpture, Plaque, Mural, Monument, other
     //    var markerTintColor: UIColor  {
@@ -31,15 +32,27 @@ class MapClass: NSObject, MKAnnotation {
     //        }
     //    }
     
-    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+    init(title: String, locationName: String, coordinate: CLLocationCoordinate2D) {
         self.title = title
         self.locationName = locationName
-        self.discipline = discipline
+        //self.discipline = discipline
         self.coordinate = coordinate
         
         super.init()
     }
-
+    //MARK: - Convert json to elements in array
+    init?(json: [Any]) {
+        // Converting strings
+        self.title = json[0] as? String ?? "No Title"
+        self.locationName = json[1] as! String
+        // Converting coordinates from string to MapClass
+        if let latitude = Double(json[2] as! String),
+            let longitude = Double(json[3] as! String) {
+            self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        } else {
+            self.coordinate = CLLocationCoordinate2D()
+        }
+    }
     
     var subtitle: String? {
         return locationName
--- E-Car parking/ViewController.swift
+++ E-Car parking/ViewController.swift
@@ -20,10 +20,24 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         //Sets the default location. This to test the app.
-        let initialLocation = CLLocation(latitude: 55.7137865, longitude: 12.4275172)
+        let initialLocation = CLLocation(latitude: 55.742527, longitude: 12.317348)
         centerMapOnLocation(location: initialLocation)
+       
+        mapView.delegate = self
+        loadInitialData()
+        mapView.addAnnotations(pElements)
+        
+//        //Testing with info on th map
+//
+//                // show artwork on map
+//                let element = MapClass(title: "Ezenta",locationName: "Hørkær 14",coordinate: CLLocationCoordinate2D(latitude: 55.717771, longitude: 12.437287))
+//                mapView.addAnnotation(element)
+        
+        
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,6 +47,28 @@ class ViewController: UIViewController, MKMapViewDelegate {
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius, regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    //Coverting JSON data into array used for annoneations in mapview
+    func loadInitialData() {
+        // 1
+        guard let fileName = Bundle.main.path(forResource: "elements", ofType: "json")
+            else { return }
+        let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
+        
+        guard
+            let data = optionalData,
+            // 2
+            let json = try? JSONSerialization.jsonObject(with: data),
+            // 3
+            let dictionary = json as? [String: Any],
+            // 4
+            let works = dictionary["data"] as? [[Any]]
+            else { return }
+        // 5
+        let validWorks = works.flatMap { MapClass(json: $0) }
+        pElements.append(contentsOf: validWorks)
+        print("print")
+        print(pElements[0].locationName)
     }
 }
 
