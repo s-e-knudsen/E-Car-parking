a80c9be884e37ebfa12756c6617ea6e124150529
SorenEKnudsen <soren.egede.knudsen@gmail.com>
2018-04-05 12:41:38 +0200

    Comment before adding SVprogressHUD.

---
+++ E-Car parking/Controller/LoginViewController.swift
@@ -0,0 +1,61 @@
+//
+//  LoginViewController.swift
+//  E-Car parking
+//
+//  Created by Søren Knudsen on 05/04/2018.
+//  Copyright © 2018 Søren Knudsen. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    
+    //MARK: - Variables
+    
+    //Outlets definition
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfeald: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        //action defines here.
+        //SVProgressHUD.show()
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfeald.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+                //SVProgressHUD.dismiss()
+                //Popup for wrong username or password.
+                let alert = UIAlertController(title: "Wrong username or password enterd", message: nil, preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default) { (action) in
+                }
+                alert.addAction(action)
+                self.present(alert, animated: true, completion: nil)
+                
+            } else {
+                print("Login success")
+                //SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToApp", sender: self)
+                
+            }
+            
+        }
+        
+    }
+    
+  
+}
---
+++ E-Car parking/Controller/WelcomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WelcomeViewController.swift
+//  E-Car parking
+//
+//  Created by Søren Knudsen on 04/04/2018.
+//  Copyright © 2018 Søren Knudsen. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
