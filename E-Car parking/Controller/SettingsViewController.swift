e7e94dd863445f969fbdd6487e9c5fbf89546ed0
SorenEKnudsen <soren.egede.knudsen@gmail.com>
2018-04-11 07:51:23 +0200

    Grafical changes and icons added.

---
+++ E-Car parking/Controller/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  E-Car parking
+//
+//  Created by Søren Knudsen on 07/04/2018.
+//  Copyright © 2018 Søren Knudsen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
